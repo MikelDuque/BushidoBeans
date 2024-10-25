3f6891892ed15e2042cf094503f274041d8084d3
Mikelpladu <mikel.0205.navarra@gmail.com>
2024-10-25 18:16:48 +0200

    Comienzo del proyecto
    ok

--- Backend/eCommerce/Program.cs
+++ Backend/eCommerce/Program.cs
@@ -1,48 +1,60 @@
-var builder = WebApplication.CreateBuilder(args);
+using eCommerce.Controllers;
+using eCommerce.Models.Database;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+namespace eCommerce;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static async Task Main(string[] args) {
+        //Especificamos el directorio de trabajo
+        Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+        
+        //Constructor
+        var builder = WebApplication.CreateBuilder(args);
+
+        //Controladores
+        builder.Services.AddControllers();
+        //Swagger/OpenApi
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        //Contextos
+        builder.Services.AddScoped<DataContext>();
+        builder.Services.AddScoped<UnitOfWork>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        //Habilita la autenticación
+        app.UseAuthentication();
+        //Habilita la autorización
+        app.UseAuthorization();
+        app.MapControllers();
+
+        //Llamamos al método de creación de base de datos de respaldo (seed)
+        await SeedDatabase(app.Services);
+
+        await app.RunAsync();
+    }
+
+    static async Task SeedDatabase(IServiceProvider serviceProvider)
+    {
+        using IServiceScope scope = serviceProvider.CreateScope();
+        using DataContext dbContext = scope.ServiceProvider.GetService<DataContext>();
+
+        if (dbContext.Database.EnsureCreated())
+        {
+            Seeder seeder = new Seeder(dbContext);
+            await seeder.SeedAsync();
+        }
+    }
 }
+    
 
-app.UseHttpsRedirection();
 
-/*
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-*/
-
-app.Run();
-
-/*
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-*/
