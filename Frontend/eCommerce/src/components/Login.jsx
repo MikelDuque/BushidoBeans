b0a3e57f742124b6be073f9271fb2a3b525906dc
Yasir <y.asirbelmaalem30@gmail.com>
2024-10-25 19:45:11 +0200

    actualizacion css-v1
    Añadido logo, boton crear cuenta, texto, recuadro verde basado en diseño figma

--- Frontend/eCommerce/src/components/Login.jsx
+++ Frontend/eCommerce/src/components/Login.jsx
@@ -1,11 +1,19 @@
 import './Login.css'
 import { useRef, useState } from "react";
+import logo from "../../public/recursos/logo.svg"
 // import jwt_decode from "jwt-decode"; // asegúrate de tener instalada la librería jwt-decode
 
 function Login(){
     const emailRef = useRef();
     const passwordRef = useRef();
+    const [emailError, setEmailError] = useState(null);
+    const [passwordError, setPasswordError] = useState(null);
+    const [promesaError, setPromesaError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    
+
     return(
+        <div className='container'>
         <div className='login'>
             <p className='accede'>Accede a tu cuenta</p>
             <p className='usaGmail'>Usa tu Gmail y contraseña</p>
@@ -20,6 +28,13 @@ function Login(){
                 </div>
             </form>
             <button className='Acceder' >Acceder</button>
+        </div>
+        <div className='crearCuenta'>
+            <img src={logo} alt="Bushido Beans" className='logoBushidoBeans' />
+            <p className='preguntaCuenta'>¿Aun no tienes cuenta?</p>
+            <p className='crearAhora'>Crea tu cuenta ahora</p>
+            <button className='btnCrearCuenta'>Crear cuenta</button>
+        </div>
         </div>
     )
 }
