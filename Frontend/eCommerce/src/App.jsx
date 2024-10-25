8c76aaa3ca58d61ae9661a9da8d534795f50a901
Yasir <y.asirbelmaalem30@gmail.com>
2024-10-25 16:38:25 +0200

    importar fuentes y colores

--- Frontend/eCommerce/src/App.jsx
+++ Frontend/eCommerce/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import InicioSesion from './components/InicioSesion.jsx'
 function App() {
 
   return (
     <>
-
+     <InicioSesion></InicioSesion>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ Frontend/eCommerce/src/components/InicioSesion.jsx
@@ -0,0 +1,9 @@
+function InicioSesion(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default InicioSesion;
