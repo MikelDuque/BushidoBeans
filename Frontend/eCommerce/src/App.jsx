91220f60b1a49eb5c95b9dda39ac97a614695fb3
Mikelpladu <mikel.0205.navarra@gmail.com>
2024-10-28 10:07:40 +0100

    Update App.jsx

--- Frontend/eCommerce/src/App.jsx
+++ Frontend/eCommerce/src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from "framer-motion"
 
 function App() {
+    const location = useLocation(); // Detecta la ruta actual
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+                <Route 
+                    path="/" 
+                    element={
+                        <motion.div
+                            initial={{ opacity: 0, x: -100 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            <Login />
+                        </motion.div>
+                    }
+                />
+                <Route 
+                    path="/register" 
+                    element={
+                        <motion.div
+                            initial={{ opacity: 0, x: 100 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            <Register />
+                        </motion.div>
+                    }
+                />
+            </Routes>
+        </AnimatePresence>
+    )
 }
 
-export default App
+export default App;
