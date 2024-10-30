4577ef54726063d7a96df5941a6f44f51cf1d4eb
Yasir <y.asirbelmaalem30@gmail.com>
2024-10-30 11:51:16 +0100

    Añadido de componente alerta y solucion de errores

--- Frontend/eCommerce/src/pages/Login.jsx
+++ Frontend/eCommerce/src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import * as jwt_decode from 'jwt-decode';
-import "../styles/login.css"
+import "../styles/login.css";
 import { useRef, useState } from "react";
 import logo from "../../public/logo.svg";
 import { validation } from '../utils/validationForm';
 import { useNavigate } from 'react-router-dom';
+import Alert from './../components/Alerta';
 
 function Login() {
     const emailRef = useRef(null);
@@ -12,45 +13,35 @@ function Login() {
     const [passwordError, setPasswordError] = useState(null);
     const [promesaError, setPromesaError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    const [alertMessage, setAlertMessage] = useState(null);
 
     const navigate = useNavigate();
 
-    const handleCrearCuenta = () =>{
-        navigate('/register');
-    }
+    const handleCrearCuenta = () => navigate('/register');
+
     const fetchingData = async (url, data) => {
+        setIsLoading(true);
         try {
-            setIsLoading(true);
             const response = await fetch(url, {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data),
             });
 
             if (response.ok) {
-                const dataPromesa = await response.json();
-                console.log("dataPromesa",dataPromesa);
-                const token = dataPromesa.accessToken;
-                console.log("token", token);
-                const decoded = jwt_decode.jwtDecode(token);
-                console.log("decoded", decoded);
-
-                if (decoded) {
-                    const userInfo = {
-                        Mail: decoded.email,
-                        Admin: decoded.rol   
-                    };
-                    console.log(userInfo);
-                }
-
-                setPromesaError(null);
+                const { accessToken } = await response.json();
+                const { email, rol: admin } = jwt_decode.jwtDecode(accessToken);
+                console.log({ email, admin });
+                setAlertMessage("Te has logeado correctamente!");
+                resetForm();
+                navigate('/');
             } else {
-                setPromesaError((await response.text()).toString);
+                setPromesaError(await response.text());
+                setAlertMessage("Error: Credenciales incorrectas.");
             }
         } catch (error) {
             setPromesaError(error.message);
+            setAlertMessage("Error: Problema de conexión.");
         } finally {
             setIsLoading(false);
         }
@@ -58,72 +49,71 @@ function Login() {
 
     const handleAcceder = async (event) => {
         event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
 
-        const emailValue = emailRef.current.value;
-        const passwordValue = passwordRef.current.value;
-
-        if (!validation.isValidEmail(emailValue)) {
+        if (!validation.isValidEmail(email)) {
             setEmailError("Por favor, introduce un formato de email válido.");
             return;
-        } else {
-            setEmailError(null);
         }
+        setEmailError(null);
 
-        if (!validation.isValidPassword(passwordValue)) {
+        if (!validation.isValidPassword(password)) {
             setPasswordError("Por favor, introduce un formato de contraseña válido.");
             return;
-        } else {
-            setPasswordError(null);
         }
+        setPasswordError(null);
 
-        const objetoBackend = {
-            Mail: emailValue,
-            Password: passwordValue,
-        };
-        console.log(objetoBackend);
-
-        await fetchingData("https://localhost:7015/api/Auth", objetoBackend);
-        alert("Te has logeado👍.")
-        resetForm();
-        navigate('/');
+        await fetchingData("https://localhost:7015/api/Auth", { Mail: email, Password: password });
     };
 
-    function resetForm() {
+    const resetForm = () => {
         emailRef.current.value = "";
         passwordRef.current.value = "";
-    }
+    };
 
     return (
-    <div className="container-supremo">
-        <div className='container'>
-            <div className='login'>
-                <p className='accede'>Accede a tu cuenta</p>
-                <p className='usaGmail'>Usa tu email y contraseña</p>
-                <form className='formLogin' onSubmit={handleAcceder}>
-                    <div className='contenedorEmail'>
-                        <label htmlFor="email" />
-                        <input type="email" name="email" id="email" ref={emailRef} placeholder='Email' />
-                        {emailError && <p className="email-message">{emailError}</p>}
-                    </div>
-                    <div className='contenedorPassword'>
-                        <label htmlFor="password" />
-                        <input type="password" name="password" id="password" ref={passwordRef} placeholder='Contraseña' />
-                        {passwordError && <p className="password-message">{passwordError}</p>}
-                    </div>
-                    <button type="submit" disabled={isLoading} className='Acceder'>
-                        {isLoading ? 'Cargando...' : 'Acceder'}
-                    </button>
-                    {promesaError && <p className="error-message">{promesaError}</p>}
-                </form>
-            </div>
-            <div className='crearCuenta'>
-                <img src={logo} alt="Bushido Beans" className='logoBushidoBeans' />
-                <p className='preguntaCuenta'>¿Aún no tienes cuenta?</p>
-                <p className='crearAhora'>Crea tu cuenta ahora</p>
-                <button className='btnCrearCuenta' onClick={handleCrearCuenta}>Crear cuenta</button>
+        <div className="container-supremo">
+            <div className="container">
+                <div className="login">
+                    <p className="accede">Accede a tu cuenta</p>
+                    <p className="usaGmail">Usa tu email y contraseña</p>
+                    <form className="formLogin" onSubmit={handleAcceder}>
+                        <div className="contenedorEmail">
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                ref={emailRef}
+                                placeholder="Email"
+                            />
+                            {emailError && <p className="email-message">{emailError}</p>}
+                        </div>
+                        <div className="contenedorPassword">
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                ref={passwordRef}
+                                placeholder="Contraseña"
+                            />
+                            {passwordError && <p className="password-message">{passwordError}</p>}
+                        </div>
+                        <button type="submit" disabled={isLoading} className="Acceder">
+                            {isLoading ? 'Cargando...' : 'Acceder'}
+                        </button>
+                        {promesaError && <p className="error-message">{promesaError}</p>}
+                    </form>
+                </div>
+                <div className="crearCuenta">
+                    <img src={logo} alt="Bushido Beans" className="logoBushidoBeans" />
+                    <p className="preguntaCuenta">¿Aún no tienes cuenta?</p>
+                    <p className="crearAhora">Crea tu cuenta ahora</p>
+                    <button className="btnCrearCuenta" onClick={handleCrearCuenta}>Crear cuenta</button>
+                </div>
+                {alertMessage && <Alert message={alertMessage} onClose={() => setAlertMessage(null)} />}
             </div>
         </div>
-    </div>
     );
 }
 
