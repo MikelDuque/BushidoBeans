f7bdb85dc2fd4835fdc918df82e5ded18983954d
MrAndrino <d.andfer96@gmail.com>
2024-11-12 19:00:33 +0100

    Endpoint?

--- Backend/eCommerce/Controllers/ProductController.cs
+++ Backend/eCommerce/Controllers/ProductController.cs
@@ -1,7 +1,6 @@
-using System;
+using eCommerce.Models.Dtos;
 using eCommerce.Services;
 using Microsoft.AspNetCore.Mvc;
-using eCommerce.Models.Dtos;
 
 namespace eCommerce.Controllers;
 
@@ -9,29 +8,30 @@ namespace eCommerce.Controllers;
 [Route("api/[controller]")]
 public class ProductController : ControllerBase
 {
-  private readonly ProductService _service;
+    private readonly ProductService _service;
 
-  public ProductController(ProductService service)
-  {
-    _service = service;
-  }
+    public ProductController(ProductService service)
+    {
+        _service = service;
+    }
 
-  /*
-  [HttpGet]
-  public async Task<IEnumerable<ProductDto>> GetAllAsync()
-  {
-    return await _service.GetAllAsync();
-  }
-  */
-  [HttpGet("{id}")]
-  public async Task<ProductDto> GetByIdAsync(long id)
-  {
-    return await _service.GetByIdAsync(id);
-  }
-  
-  [HttpGet("Filtered_Products")]
-  public async Task<Catalog> GetFilteredProducts(Filter filter)
-  {
-    return await _service.GetFilteredProducts(filter);
-  }
+    /*
+    [HttpGet]
+    public async Task<IEnumerable<ProductDto>> GetAllAsync()
+    {
+      return await _service.GetAllAsync();
+    }
+    */
+
+    [HttpGet("{id}")]
+    public async Task<ProductDto> GetByIdAsync(long id)
+    {
+        return await _service.GetByIdAsync(id);
+    }
+
+    [HttpGet("Filtered_Products")]
+    public async Task<Catalog> GetFilteredProducts(Filter filter)
+    {
+        return await _service.GetFilteredProducts(filter);
+    }
 }
