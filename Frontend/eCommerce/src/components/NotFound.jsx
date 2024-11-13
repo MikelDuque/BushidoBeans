9c3990875ad614bb5a4edcb74c3ad4d5296e2d11
Yasir <y.asirbelmaalem30@gmail.com>
2024-11-13 18:55:34 +0100

    creacion de notfound

--- Frontend/eCommerce/src/components/NotFound.jsx
+++ Frontend/eCommerce/src/components/NotFound.jsx
@@ -1,11 +1,21 @@
-// NotFound.js
 import React from 'react';
-
+import Header from './Header';
+import Footer from './Footer';
 const NotFound = () => {
+    const navigate = useNavigate();
+
+    const handleGoHome = () => {
+        navigate('/');
+    };
     return (
-        <div>
-            <h2>404 - Producto no encontrado</h2>
-            <p>El producto que buscas no existe.</p>
+        <div className='container-not-found'>
+            <Header/>
+            <p className='titulo texto-not-found'>ERROR 404</p>
+            <p className='subtitulo texto-buscar-oro'>Vaya, viniste buscando oro y encontraste... ¡nada!</p>
+            <p className='texto texto-pagina-construccion'>Esta pagina se encuentra actualmente en construccion, disculpa las molestias</p>
+            <button onClick={handleGoHome}>Ir a la pagina principal</button>
+            <img src="" alt="" />
+            <Footer/>
         </div>
     );
 };
