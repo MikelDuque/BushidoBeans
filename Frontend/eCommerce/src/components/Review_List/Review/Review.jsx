de5e3c147f51c36adf4f3cdf3fc00325750cc990
Mikelpladu <mikel.0205.navarra@gmail.com>
2024-11-19 11:19:50 +0100

    Creación componente "product_details"

--- Frontend/eCommerce/src/components/Review_List/Review/Review.jsx
+++ Frontend/eCommerce/src/components/Review_List/Review/Review.jsx
@@ -5,7 +5,6 @@ function printScore(score) {
   return Array(score).fill(
     <img className={classes.review__individualImg} src="https://cdn.pixabay.com/photo/2023/08/28/01/23/star-8218104_1280.png" alt="review star" />
   )
-
 }
 
 function dateFormatting(date) {
@@ -15,12 +14,10 @@ function dateFormatting(date) {
 function Review({reviewData}) {
   const [displayFullBody, setDisplayFullBody] = useState(false);
 
-  function showFullBody() {
-    setDisplayFullBody(true);
-  }
+  function showFullBody() { setDisplayFullBody(true);};
 
-  function bodySizing() {
-    if (reviewData.body.length < 150 || displayFullBody == false) {
+  function bodyConstructor() {
+    if (reviewData.body.length < 300 || displayFullBody == false) {
       const subBody = reviewData.body.substring(0, 149);
 
       return (
@@ -32,7 +29,7 @@ function Review({reviewData}) {
     };
 
     return <p className={classes.review__body}>{reviewData.body}</p>
-  }
+  };
 
   const avatar =  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/315b1136-d62f-4ef9-aeeb-bf1738eba7e0/de9feyk-f10bf0db-7b73-4cb1-bb38-b0db7cdb1dba.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzMxNWIxMTM2LWQ2MmYtNGVmOS1hZWViLWJmMTczOGViYTdlMFwvZGU5ZmV5ay1mMTBiZjBkYi03YjczLTRjYjEtYmIzOC1iMGRiN2NkYjFkYmEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.SCl_4YhlDQDLf8WAU1gq0tmzq1nah0wb7YCI7d0qim8";
   const avatar2 = `https://localhost:7015${reviewData.avatar}`
@@ -48,8 +45,7 @@ function Review({reviewData}) {
           </figure>
         </div>
         <p>{dateFormatting(reviewData.publicationDate)}</p>
-        {bodySizing()}
-        {/*<p className={classes.review__body}>{reviewData.body}</p>*/}
+        {bodyConstructor()}
       </div>
     </li>
   );
--- Frontend/eCommerce/src/components/Review_List/Review_List.jsx
+++ Frontend/eCommerce/src/components/Review_List/Review_List.jsx
@@ -1,58 +1,9 @@
 import classes from './Review_List.module.css';
-import Review from './Review/Review';
-import Average_Score from './Average_Score/Average_Score';
-import Modal from './../Pop-Up.jsx';
-import PopupReseña from './../PopUpReseña.jsx';
+import Review from './Review/Review.jsx';
+import Average_Score from './Average_Score/Average_Score.jsx';
+import Modal from '../Pop-Up.jsx';
+import PopupReseña from '../PopUpReseña.jsx';
 import { useState } from 'react';
-
-//----- CONSTRUCTOR -----//
-function ReviewObj(productId, userId, score, body) {
-  if (score === undefined) {
-    throw new Error("Elige una puntuación");
-  }
-
-  return {
-    productId,
-    userId,
-    score,
-    body: body || ""
-  };
-}
-
-
-
-//----- OBJETO DE PRUEBA -----//
-const productId = 1;
-const userId = 2;
-const score = 1;
-const body = "Más malo que pegarle a un padre con un calcetín sudado";
-
-
-//----- FUNCIÓN PARA MANDAR LA REVIEW AL BACK -----//
-
-const newReview = ReviewObj(productId, userId, score, body);
-
-const sendReview = async () => {
-  try {
-    const response = await fetch("https://localhost:7015/api/InsertReview", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newReview)
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      console.log("Review enviada exitosamente:", data);
-      
-    } else {
-      console.error("Error al enviar la review:", response.statusText);
-    }
-  } catch (error) {
-    console.error("Error de red:", error);
-  }
-};
 
 function reviewMapper(reviews) {
   return (reviews.length > 0 ? (
@@ -91,7 +42,6 @@ function Review_List({ data }) {
       </Modal>
     </div>
   );
-  
 };
 
 export default Review_List;
