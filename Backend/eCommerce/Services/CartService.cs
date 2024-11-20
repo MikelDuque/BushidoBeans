fb07ebab1d75a520f53def539325a7b51e33a08c
Yasir <y.asirbelmaalem30@gmail.com>
2024-11-20 11:44:54 +0100

    commit de lo ultimo

--- Backend/eCommerce/Services/CartService.cs
+++ Backend/eCommerce/Services/CartService.cs
@@ -1,7 +1,6 @@
 using eCommerce.Controllers;
 using eCommerce.Models.Database.Entities;
 using eCommerce.Models.Database.Repositories;
-using eCommerce.Models.Dtos;
 
 namespace eCommerce.Services;
 
@@ -10,12 +9,13 @@ public class CartService
     private readonly UnitOfWork _unitOfWork;
     private readonly CartRepository _cartRepository;
     private readonly ProductService _productService;
+    private readonly UnitOfWork _unitOfWork;
+    private readonly CartRepository _cartRepository;
+    private readonly ProductService _productService;
 
-    public CartService(UnitOfWork unitOfWork, CartRepository cartRepository, ProductService productService)
+    public CartService(UnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
-        _cartRepository = cartRepository;
-        _productService = productService;
     }
 
     public async Task<Cart?> GetCartAsync(long userId)
@@ -23,38 +23,39 @@ public class CartService
         return await _cartRepository.GetCartByUserIdAsync(userId);
     }
 
-    public async Task<Cart> AddToCartAsync(CartProductDto cartProductDto)
+    public async Task<Cart> AddToCartAsync(long userId, long productId, int quantity)
     {
-        var cart = await GetCartAsync(cartProductDto.UserId);
+        var cart = await GetCartAsync(userId);
 
         if (cart == null)
         {
             cart = new Cart
             {
-                UserId = cartProductDto.UserId,
+                UserId = userId,
                 CartProducts = new List<CartProduct>()
             };
            _cartRepository.AddCart(cart);
         }
 
-        var product = await _productService.GetByIdAsync(cartProductDto.ProductId);
+        var product = await _productService.GetByIdAsync(productId);
         if (product == null)
         {
             throw new Exception("Producto no encontrado");
         }
 
-        var existingItem = cart.CartProducts.FirstOrDefault(cp => cp.ProductId == cartProductDto.ProductId);
+        var existingItem = cart.CartProducts.FirstOrDefault(cp => cp.ProductId == productId);
 
         if (existingItem != null)
         {
-            existingItem.Quantity += cartProductDto.Quantity;
+            existingItem.Quantity += quantity;
         }
         else
         {
             cart.CartProducts.Add(new CartProduct
             {
-                ProductId = cartProductDto.ProductId,
-                Quantity = cartProductDto.Quantity
+                CartId = cart.Id,
+                ProductId = productId,
+                Quantity = quantity
             });
         }
 
