182b3a691d6cde642feec5c0bbaa083d0c4b39d4
Yasir <y.asirbelmaalem30@gmail.com>
2024-11-20 10:50:04 +0100

    commit de lo poco que llevo

--- Backend/eCommerce/Controllers/CartController.cs
+++ Backend/eCommerce/Controllers/CartController.cs
@@ -1,5 +1,3 @@
-﻿using eCommerce.Models.Dtos;
-using eCommerce.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,35 +7,5 @@ namespace eCommerce.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
-        private readonly CartService _cartService;
-
-        public CartController(CartService cartService) { 
-            _cartService = cartService;
-        }
-        [HttpGet("{userId}")]
-        public async Task<IActionResult> GetCart(long userId) { 
-            var cart = await _cartService.GetCartAsync(userId);
-            if (cart == null) { return NotFound(); }
-            return Ok(cart);
-        }
-
-        [HttpPost("{userId}/add")]
-        public async Task<IActionResult> AddToCart([FromBody] CartProductDto cartProductDto)
-        {
-            var cart = await _cartService.AddToCartAsync(cartProductDto);
-            return Ok(cart);
-        }
-
-        [HttpPut("{userId}/update")]
-        public async Task<IActionResult> UpdateCart(long userId, [FromBody] CartProductDto cartProductDto)
-        {
-            var cart = await _cartService.UpdateCartItemsAsync(userId, cartProductDto.ProductId, cartProductDto.Quantity);
-            if (cart == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(cart);
-        }
     }
 }
