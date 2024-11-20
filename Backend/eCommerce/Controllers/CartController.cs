dd5f9bacf20da3eaf68d18412beca38baab395af
IvanMontesGuti <162334629+IvanMontesGuti@users.noreply.github.com>
2024-11-20 15:59:22 +0100

    CartConroller 1

---
+++ Backend/eCommerce/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using eCommerce.Models.Database.Entities;
+using eCommerce.Models.Dtos;
+using eCommerce.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly CartService _cartService;
+
+        public CartController(CartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<CartDto> GetCartByIdAsync(long id)
+        {
+            return await _cartService.GetCartByIdAsync(id);
+        }
+
+        [Authorize]
+        [HttpPost("Insert_CartProduct")]
+        public async Task<ActionResult<CartProduct>> CreateReviewAsync([FromQuery] CartProduct cartProduct)
+        {
+
+            if (cartProduct == null) return BadRequest("Datos del producto no válidos.");
+
+            return await _cartService.InsertCartProductAsync(cartProduct);
+        }
+
+        [Authorize]
+        [HttpPut("Update-Cart")]
+        public async Task<ActionResult<CartProduct>> CreateReviewAsync([FromQuery] CartProduct cartProduct)
+        {
+
+            if (cartProduct == null) return BadRequest("Datos del producto no válidos.");
+
+            return await _cartService.UpdateCartProductAsync(cartProduct);
+        }
+
+    }
+}
