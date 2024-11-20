c8786513d725efcebdc538a695d4c4ee1f4be4f6
Yasir <y.asirbelmaalem30@gmail.com>
2024-11-20 11:44:54 +0100

    commit de lo ultimo

--- Backend/eCommerce/Controllers/CartController.cs
+++ Backend/eCommerce/Controllers/CartController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using eCommerce.Models.Dtos;
+using eCommerce.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.Controllers
@@ -7,5 +9,42 @@ namespace eCommerce.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
+        private readonly CartService _cartService;
+
+        public CartController(CartService cartService) { 
+            _cartService = cartService;
+        }
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetCart(long userId) { 
+            var cart = await _cartService.GetCartAsync(userId);
+            if (cart == null) { return NotFound(); }
+            return Ok(cart);
+        }
+
+        [HttpPost("{userId}/add")]
+        public async Task<IActionResult> AddToCart(long userId, long ProductId, int Quantity)
+        {
+            try
+            {
+                var cart = await _cartService.AddToCartAsync(userId,ProductId,Quantity);
+                return Ok(cart);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPut("{userId}/update")]
+        public async Task<IActionResult> UpdateCart(long userId, [FromBody] CartProductDto cartProductDto)
+        {
+            var cart = await _cartService.UpdateCartItemsAsync(userId, cartProductDto.ProductId, cartProductDto.Quantity);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cart);
+        }
     }
 }
--- Backend/eCommerce/Models/Database/Entities/CartProduct.cs
+++ Backend/eCommerce/Models/Database/Entities/CartProduct.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eCommerce.Models.Database.Entities;
 
