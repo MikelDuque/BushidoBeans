8b82f8d99661d808009532ef881fcd0d001a0b34
IvanMontesGuti <162334629+IvanMontesGuti@users.noreply.github.com>
2024-11-21 11:47:39 +0100

    Authorize crud

--- Backend/eCommerce/Controllers/CartController.cs
+++ Backend/eCommerce/Controllers/CartController.cs
@@ -2,10 +2,13 @@
 using eCommerce.Models.Dtos;
 using eCommerce.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
 
 namespace eCommerce.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CartController : ControllerBase
@@ -17,27 +20,41 @@ namespace eCommerce.Controllers
             _cartService = cartService;
         }
 
-        //[Authorize]
+        [Authorize]
         [HttpGet("Get_Cart")]
         public async Task<CartDto> GetCartByIdAsync(long id)
         {
+            Claim userClaimId = User.FindFirst("id");
+
+            if (userClaimId == null) Unauthorized("Usuario no autorizado");
+
             return await _cartService.GetCartAsync(id);
+
         }
 
 
-        //[Authorize]
+        [Authorize]
         [HttpPut("Update_CartProduct")]
         public ActionResult<CartProduct> UpdateCart([FromQuery] CartProduct cartProduct)
         {
+
+            Claim userClaimId = User.FindFirst("id");
+
+            if (userClaimId == null) Unauthorized("Usuario no autorizado");
 
             if (cartProduct == null) return BadRequest("Datos del producto no válidos.");
 
             return  Ok (_cartService.UpdateCartProductAsync(cartProduct));
         }
 
+        [Authorize]
         [HttpDelete("Delete_CartProduct")]
         public async Task<ActionResult> DeleteCartProduct([FromQuery] CartProduct cartProduct)
         {
+            Claim userClaimId = User.FindFirst("id");
+
+            if (userClaimId == null) Unauthorized("Usuario no autorizado");
+
             try
             {
                 await _cartService.DeleteCartProduct(cartProduct); 
@@ -50,10 +67,14 @@ namespace eCommerce.Controllers
             return NoContent();
         }
 
+        [Authorize]
         [HttpGet("Update_GlobalCart")]
         public async Task<List<CartProduct>> GetCartAsync([FromQuery]List<CartProduct> cartProduct)
         {
-       
+            Claim userClaimId = User.FindFirst("id");
+
+            if (userClaimId == null) Unauthorized("Usuario no autorizado");
+
             await _cartService.UpdateCartProductsAsync(cartProduct);
             return cartProduct;
             
