803eef1965b3cb3cfc2eeee59a1c9d93f70237a4
IvanMontesGuti <162334629+IvanMontesGuti@users.noreply.github.com>
2024-11-26 17:15:20 +0100

    Primer punto control controladores y sevicio y repositorio de order

---
+++ Backend/eCommerce/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using eCommerce.Models.Database.Entities;
+using eCommerce.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace eCommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderService _orderService;
+        public OrderController(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetOrderByIdAsync(long id)
+        {
+            return Ok(await _orderService.GetOrderAsync(id));
+        }
+
+        /*
+
+        [HttpPost("Update_Cart")]
+        public async Task<ActionResult> UpdateCartAsync([FromBody] List<CartProduct> cartProducts)
+        {
+            Claim userClaimId = User.FindFirst("id");
+
+            if (userClaimId == null) return Unauthorized("Usuario no autorizado");
+
+            await _cartService.UpdateCartProductsAsync(cartProducts);
+
+            return Ok(cartProducts);
+
+        }
+        */
+    }
+}
---
+++ Backend/eCommerce/Services/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using eCommerce.Controllers;
+using eCommerce.Models.Database.Entities;
+using eCommerce.Models.Dtos;
+using eCommerce.Models.Mappers;
+
+namespace eCommerce.Services
+{
+    public class OrderService
+    {
+        private readonly UnitOfWork _unitOfWork;
+        private readonly OrderMapper _orderMapper;
+
+        public OrderService(UnitOfWork unitOfWork, OrderMapper orderMapper)
+        {
+            _unitOfWork = unitOfWork;
+            _orderMapper = orderMapper;
+        }
+
+        public async Task<List<OrderDto>> GetOrderAsync(long userId)
+        {
+            User user = await _unitOfWork.UserRepository.GetByIdAsync(userId);
+            return _orderMapper.ToDto(user.Orders).ToList();
+        }
+
+    }
+
+
+}
