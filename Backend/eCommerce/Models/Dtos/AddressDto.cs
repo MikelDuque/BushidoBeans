ad7df2ec0e94c0d61b101dc1b338f6cb057227f5
Yasir <y.asirbelmaalem30@gmail.com>
2024-11-27 09:58:08 +0100

    commit(no te borres porfavor)

---
+++ Backend/eCommerce/Models/Dtos/AddressDto.cs
@@ -0,0 +1,11 @@
+﻿namespace eCommerce.Models.Dtos
+{
+    public class AddressDto
+    {
+        public string Addressee { get; set; }
+        public long PhoneNumber { get; set; }
+        public string NameAddress { get; set; }
+        public long UserId { get; set; }
+
+    }
+}
---
+++ Backend/eCommerce/Models/Mappers/AddressMapper.cs
@@ -0,0 +1,19 @@
+﻿using eCommerce.Models.Database.Entities;
+using eCommerce.Models.Dtos;
+
+namespace eCommerce.Models.Mappers
+{
+    public class AddressMapper
+    {
+        public AddressDto ToDto(Address address)
+        {
+            return new AddressDto
+            {
+                Addressee = address.Addressee,
+                PhoneNumber = address.PhoneNumber,
+                NameAddress = address.NameAddress,
+                UserId = address.Id
+            };
+        }
+    }
+}
