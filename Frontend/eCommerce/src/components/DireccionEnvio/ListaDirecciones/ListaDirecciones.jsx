449036d977d526a5a461bacb0bf2aab0da6c77aa
MrAndrino <d.andfer96@gmail.com>
2024-11-29 01:49:29 +0100

    Funcionalidad Checkout

--- Frontend/eCommerce/src/components/DireccionEnvio/ListaDirecciones/ListaDirecciones.jsx
+++ Frontend/eCommerce/src/components/DireccionEnvio/ListaDirecciones/ListaDirecciones.jsx
@@ -1,24 +1,27 @@
+import { useCheckout } from "../../../context/CheckoutContext";
 import "./ListaDirecciones.css";
 
 function ListaDirecciones({ direcciones }) {
-    return (
-        <div className="container-lista-direcciones">
-            <h4 className="titulo-direcciones">Lista de Direcciones</h4>
-            {direcciones.length > 0 ? (
-                <ul className="lista-direcciones">
-                    {direcciones.map((direccion, index) => (
-                        <li className="item-direccion" key={index}>
-                            <p>{direccion.nombre}</p>
-                            <p>{direccion.calle}, {direccion.ciudad} ({direccion.provincia}), {direccion.codigopostal}</p>
-                            <p>{direccion.telefono}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p className="texto lista-vacia">No tienes direcciones guardadas.</p>
-            )}
-        </div>
-    );
+  const { handleButtonClick } = useCheckout();
+
+  return (
+    <div className="container-lista-direcciones">
+      <h4 className="titulo-direcciones">Lista de Direcciones</h4>
+      {direcciones.length > 0 ? (
+        <ul className="lista-direcciones">
+          {direcciones.map((direccion, index) => (
+            <li className="item-direccion" key={index} onClick={() => handleButtonClick('confirm')}>
+              <p>{direccion.nombre}</p>
+              <p>{direccion.calle}, {direccion.ciudad} ({direccion.provincia}), {direccion.codigopostal}</p>
+              <p>{direccion.telefono}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="texto lista-vacia">No tienes direcciones guardadas.</p>
+      )}
+    </div>
+  );
 }
 
 export default ListaDirecciones;
