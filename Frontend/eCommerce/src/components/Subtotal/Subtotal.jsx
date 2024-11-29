fd67a7f12d682d5e87fe54337837e079ff84df26
Mikelpladu <mikel.0205.navarra@gmail.com>
2024-11-29 16:28:24 +0100

    Avance order funcionalidad

--- Frontend/eCommerce/src/components/Subtotal/Subtotal.jsx
+++ Frontend/eCommerce/src/components/Subtotal/Subtotal.jsx
@@ -1,11 +1,10 @@
 import "./Subtotal.css";
 import { useCheckout } from "../../context/CheckoutContext"; // Importa el contexto
 
-function Subtotal({ carrito, view }) {
+function Subtotal({ view }) {
+    const {handleButtonClick, calculateShipping, order} = useCheckout();
 
-    // Usa el contexto Checkout para cambiar el `currentView`
-    const {handleButtonClick} = useCheckout();
-
+    /*
     const handleSubtotal = () => {
         if (!carrito || carrito.length === 0) return 0;
         return carrito.reduce((total, item) => total + item.price * item.quantity, 0);
@@ -16,18 +15,20 @@ function Subtotal({ carrito, view }) {
         const subtotal = handleSubtotal();
         return subtotal > 35 ? 0 : 2.99;
     };
+    */
 
     // Renderiza según la vista
     return (
         <div className="container-subtotal">
             <p className="texto-subtotal titulo">SUBTOTAL</p>
-            <p className="subtitulo">{handleSubtotal().toFixed(2)} €</p> 
+            <p className="subtitulo">{order.Subtotal} €</p>
+            {/*<p className="subtitulo">{handleSubtotal().toFixed(2)} €</p>*/}
 
             {view === "direccion" && (
                 <p className="envio">
-                    {handleSubtotal() > 35
+                    {order.Subtotal > 35
                         ? "Envío gratis"
-                        : `Gastos de envío: ${calculateShipping().toFixed(2)} €`}
+                        : `Gastos de envío: ${calculateShipping()} €`}
                 </p>
             )}
 
