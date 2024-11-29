7d854ea896f3315f859db734319c1c8f8b91cf24
MrAndrino <d.andfer96@gmail.com>
2024-11-29 02:17:55 +0100

    Reapply "Merge branch 'develop' into david"
    This reverts commit 1b419d81deefee315516272919a27a77ebf4b373.

--- Frontend/eCommerce/src/components/DireccionEnvio/ListaDirecciones/ListaDirecciones.jsx
+++ Frontend/eCommerce/src/components/DireccionEnvio/ListaDirecciones/ListaDirecciones.jsx
@@ -1,14 +1,24 @@
-import { useDirecciones } from "../../../context/DireccionContext";
+import "./ListaDirecciones.css";
 
-function ListaDirecciones() {
-    const { direcciones } = useDirecciones();
-
+function ListaDirecciones({ direcciones }) {
     return (
-        <div>
-            <h2>Lista de Direcciones</h2>
-            {direcciones.map((direccion) => (
-                <CardDireccion key={direccion.id} direccion={direccion} />
-            ))}
+        <div className="container-lista-direcciones">
+            <h2 className="titulo-direcciones">Lista de Direcciones</h2>
+            {direcciones.length > 0 ? (
+                <ul className="lista-direcciones">
+                    {direcciones.map((direccion, index) => (
+                        <li className="item-direccion" key={index}>
+                            <p className="texto nombre-direccion">{direccion.nombre}</p>
+                            <p className="texto calle-direccion">{direccion.calle}</p>
+                            <p className="texto ciudad-direccion"> {direccion.ciudad}</p>
+                            <p className="texto ciudad-direccion"> {direccion.telefono}</p>
+
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p className="texto lista-vacia">No tienes direcciones guardadas.</p>
+            )}
         </div>
     );
 }
