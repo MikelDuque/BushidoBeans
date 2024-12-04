d137948957e5b0ebce65875ecb257bbb396c4e65
Mikelpladu <mikel.0205.navarra@gmail.com>
2024-12-04 09:26:57 +0100

    Vuelta al Carrito Context

--- Frontend/eCommerce/src/context/CarritoContext.jsx
+++ Frontend/eCommerce/src/context/CarritoContext.jsx
@@ -1,77 +1,312 @@
-import { createContext, useContext, useState, useEffect} from 'react';
+import { createContext, useContext, useState, useEffect, useRef } from 'react';
 import useFetch from "../endpoints/useFetch";
-import { useAuth } from './AuthContext_Refactored';
-import { GET_CART_BY_ID, PUT_CART, DELETE_CARTPRODUCT , DELETE_CART_BY_ID} from "../endpoints/config";
+import { useAuth } from './AuthContext';
+import { GET_CART_BY_ID, PUT_CART, DELETE_CARTPRODUCT , DELETE_CART_BY_ID, PUT_CARTPRODUCT} from "../endpoints/config";
+import { jwtDecode } from 'jwt-decode';
 
 
 /* ----- Preparación Contexto ----- */
 
-const CartContext = createContext();
-export const useCarrito = () => {return useContext(CartContext)};
+const CarritoContext = createContext();
+export const useCarrito = () => {return useContext(CarritoContext)};
 
 export const CarritoProvider = ({ children }) => {
+    const { isAuthenticated} = useAuth();
+    const [carrito, setCarrito] = useState([]);
+    const [token, setToken] = useState(null);
+    const [userId, setUserId] = useState(0);
 
-    /* ----- Constantes Iniciales ----- */
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    const token  = useAuth();
-
-    const localCart = localStorage.getItem('carrito') ? JSON.parse(localStorage.getItem("carrito")) : [];
-
-    const [cart, setCart] = useState(localCart);
-    //const [totalProducts, setTotalProducts] = useState(0);
-    //const totalProducts = useRef(0)
-    
-    const { fetchData:backendCart} = useFetch({
-        url: GET_CART_BY_ID(1),
-        type: "GET",
-        token: token,
-        params: 1,
-        condition: true
-    });
-
-
-    /* ----- UseEffect Inicial ----- */
-
-    useEffect(() => {
-        if(backendCart) setCart(backendCart);
-    }, [backendCart])
-    
-    /* ----- Métodos ----- */
-
-    function handleCart(newCart) {setCart(newCart)};
-
-    //TERMINAR (surge error, no toma el carrito)
-    /*
-    function handleTotalProducts() { 
-        const totalProducts2 = carrito != [] ? Array.from(carrito).reduce((total, product) => total + product.quantity, 0) : 0;
+    function setCarritoPrueba() {
+        const carritoPrueba = [];
         
-        totalProducts.current = totalProducts2;
-    }
+        if (!isAuthenticated) for (let i = 0; i < 5; i++) {
+            carritoPrueba.push({
+                id: i,
+                image: "https://pbs.twimg.com/profile_images/1859044378662027264/Km09QDjK_400x400.jpg",
+                name: "Nombre de Prueba",
+                price: 2.55,
+                stock: 20,
+                quantity: 1,
+                userId: userId,
+                productId: 1
+            })
+        }
+        
+        localStorage.setItem('carrito', JSON.stringify(carritoPrueba));
+    };
 
-    function updateCart() {
-        const {fetchData: newCart} = useFetch({
-            Url: PUT_CART,
-            type: "PUT",
-            token: token,
-            params: localCart,
-            condition: newCart
-        });
+    // Función para manejar el token y obtener el userId
+    const handleToken = () => {
+        const tokenLS = localStorage.getItem('accessToken');
+        if (!tokenLS) {
+            console.log('No hay token de autenticación');
+             
+        } else {
+            setToken(tokenLS);
+            const decodedToken = jwtDecode(tokenLS);
+            
+            if (!decodedToken.id) {
+            console.log('No se encontró el userId en el token');
 
-        handleCart(newCart);
+            } else setUserId(decodedToken.id) 
+        }; 
+    };
+
+    function handleCart(newCart) {
+        setCarrito(newCart);
     }
-    */
+ 
+    useEffect(() => {
+        handleToken();
+        setCarritoPrueba();
+
+        console.log("autenticado", isAuthenticated);
+        
+
+        if (isAuthenticated) {
+           //actualizarCarritoBackend();
+           obtenerCarritoBackend();
+
+        } else {
+            const carritoGuardado = JSON.parse(localStorage.getItem('carrito'));
+            if (carritoGuardado) {
+                handleCart(carritoGuardado);
+            }
+        }
+    }, [isAuthenticated]);
+
+    /*LLAMADAS A LOS ENDPOINT*/
+
+    const obtenerCarritoBackend = async () => {
+        setIsLoading(true);
+        try {
+            const response = await fetch(GET_CART_BY_ID(userId), {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                handleCart(data);
+            } else {
+                setError(await response.text());
+            }
+        } catch (error) {
+            setError(error.message);
+            console.log("error", error.message);
+            
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    
+
+    const actualizarCarritoBackend = async () => {
+
+        const backendCart = {
+            id: userId,
+            cartProducts: localStorage.getItem(carrito)
+        }
+
+        setIsLoading(true);
+        try {
+            const response = await fetch(PUT_CART, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify(backendCart)
+            });
+            
+
+            if (response.ok) {
+                const data = await response.json();
+                handleCart(data);
+                localStorage.removeItem('carrito');
+            } else {
+                setError(await response.text());
+            }
+        } catch (error) {
+            setError(error.message);
+            console.log("error", error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     
-    //function getTotalProducts() { console.log("carrito", carrito); return carrito != []? carrito.reduce((total, product) => total + product.quantity, 0) : 0};
-    //console.log("totalProducts", getTotalProducts());
+
+    const agregarAlCarrito = async (producto) => {
+        if (isAuthenticated) {
+            setIsLoading(true);
+            try {
+                const response = await fetch(PUT_CARTPRODUCT, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: producto
+                });
+
+                if (!response.ok) {
+                    setError(await response.text());
+                }
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+
+            try {
+                setCarrito((prevCarrito) => {
+                    const productoExistente = prevCarrito.find((item) => item.id === data.productId);
+                    if (productoExistente) {
+                        return prevCarrito.map((item) =>
+                            item.id === data.productId
+                                ? { ...item, quantity: data.quantity }
+                                : item
+                        );
+                    } else {
+                        return [
+                            ...prevCarrito,
+                            {
+                                id: data.productId,
+                                quantity: data.quantity,
+                            },
+                        ];
+                    }
+                });
+            } catch (error) {
+                console.error('Error al agregar el producto al carrito:', error);
+            }
+        } else {
+            setCarrito((prevCarrito) => {
+                const productoExistente = prevCarrito.find((item) => item.id === producto.id);
+                if (productoExistente) {
+                    const nuevoCarrito = prevCarrito.map((item) =>
+                        item.id === producto.id
+                            ? { ...item, quantity: item.quantity + producto.quantity }
+                            : item
+                    );
+                    localStorage.setItem('carrito', JSON.stringify(nuevoCarrito));
+                    return nuevoCarrito;
+                } else {
+                    const nuevoCarrito = [...prevCarrito, { ...producto }];
+                    localStorage.setItem('carrito', JSON.stringify(nuevoCarrito));
+                    return nuevoCarrito;
+                }
+            });
+        }
+    };
+
+
+    function establecerCarrito(carritoNuevo) {
+        setCarrito((prevCarrito) => {
+            const productoExistente = prevCarrito.find((item) => item.id === carritoNuevo.productId);
+            if (productoExistente) {
+                return prevCarrito.map((item) =>
+                    item.id === carritoNuevo.productId
+                        ? { ...item, quantity: carritoNuevo.quantity }
+                        : item
+                );
+            } else {
+                return [
+                    ...prevCarrito,
+                    {
+                        id: carritoNuevo.productId,
+                        quantity: carritoNuevo.quantity,
+                    },
+                ];
+            }
+        });
+    }
+
+
+
+    const eliminarDelCarrito = async (producto) => {
+        if (isAuthenticated) {
+            setIsLoading(true);
+            try {
+                const response = await fetch(DELETE_CARTPRODUCT, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: producto
+                });
+
+                if (!response.ok) {
+                    setError(await response.text());
+                }
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+
+            try {
+
+                setCarrito((prevCarrito) =>
+                    prevCarrito.filter((item) => item.id !== producto.id)
+                );
+            } catch (error) {
+                console.error('Error al eliminar el producto del carrito:', error);
+            }
+        } else {
+            const nuevoCarrito = carrito.filter((item) => item.id !== producto.id);
+            setCarrito(nuevoCarrito);
+        }
+    };
+
+    const eliminarContenidoCarrito = async () => {
+        if (isAuthenticated) {
+            setIsLoading(true);
+            try {
+                const response = await fetch(DELETE_CART_BY_ID(userId), {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: userId
+                });
+
+                if (!response.ok) {
+                    setError(await response.text());
+                }
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+
+            try {
+                setCarrito([]);
+            } catch (error) {
+                console.error('Error al eliminar el contenido del carrito:', error);
+            }
+        } else {
+            setCarrito([]);
+        }
+    };
 
 
     /* ----- Fin Context ----- */
 
     const ctxValue = {
-        cart,
-        //totalProducts: totalProducts.current,
+        carrito,
+        agregarAlCarrito,
+        eliminarDelCarrito,
+        eliminarContenidoCarrito,
     };
 
-    return <CartContext.Provider value={ctxValue}> {children} </CartContext.Provider>
+    return <CarritoContext.Provider value={ctxValue}> {children} </CarritoContext.Provider>
 };
