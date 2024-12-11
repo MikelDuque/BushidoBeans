d56d775c02ae602b2df6352cb6bd25583cd6f524
MrAndrino <d.andfer96@gmail.com>
2024-12-11 21:08:40 +0100

    SN Respon - Valores

--- Frontend/eCommerce/src/pages/SobreNosotros/SobreNosotros.jsx
+++ Frontend/eCommerce/src/pages/SobreNosotros/SobreNosotros.jsx
@@ -19,42 +19,42 @@ function SobreNosotros() {
     const imagenes = [imagen1, imagen2, imagen3, imagen4]
     return (
         <div className='container-sobre-nosotros'>
+
             <div className="carrusel"><Carrusel images={imagenes} /></div>
-            
+
             <div className='container-descripcion'>
                 <img className='imagen-logo-bushido-beans' src={logo} alt="" />
                 <div className='descripcion-texto'>
-                <p>En Bushido Beans, creemos que cada momento merece ser disfrutado con una taza de café o té de la más alta calidad. Fundada con la misión de ofrecer bebidas exquisitas y prácticas, nuestra marca está diseñada para aquellos que llevan un estilo de vida ajetreado y buscan un respiro en su día a día.</p>
-                <p>Nuestra selección incluye una variedad de cafés y tés cuidadosamente preparados, todos listos para beber y disfrutar en cualquier lugar y en cualquier momento. Ya sea que necesites un impulso de energía inmediata para afrontar una larga jornada, o simplemente desees saborear un momento de tranquilidad, Bushido Beans tiene la bebida perfecta para ti.</p>
-                <p>Cada producto que ofrecemos combina ingredientes de calidad premium con sabores únicos, presentados en formatos convenientes que se adaptan a tu ritmo de vida. Desde intensos cafés hasta delicados tés, nuestra variedad está diseñada para satisfacer todos los paladares.</p></div>
+                    <p>En Bushido Beans, creemos que cada momento merece ser disfrutado con una taza de café o té de la más alta calidad. Fundada con la misión de ofrecer bebidas exquisitas y prácticas, nuestra marca está diseñada para aquellos que llevan un estilo de vida ajetreado y buscan un respiro en su día a día.</p><br/>
+                    <p>Nuestra selección incluye una variedad de cafés y tés cuidadosamente preparados, todos listos para beber y disfrutar en cualquier lugar y en cualquier momento. Ya sea que necesites un impulso de energía inmediata para afrontar una larga jornada, o simplemente desees saborear un momento de tranquilidad, Bushido Beans tiene la bebida perfecta para ti.</p><br/>
+                    <p>Cada producto que ofrecemos combina ingredientes de calidad premium con sabores únicos, presentados en formatos convenientes que se adaptan a tu ritmo de vida. Desde intensos cafés hasta delicados tés, nuestra variedad está diseñada para satisfacer todos los paladares.</p></div>
             </div>
 
             <hr className='separador' />
 
             <div className='container-mision'>
                 <img src="/recursos/mision.jpg" alt="" className='imagen-mision' />
+
                 <div className='container-texto-mision'>
-                    <h1>Mision</h1>
-                    <p>Ofrecer una experiencia de café excepcional que inspire y conecte, respetando los principios de calidad, sostenibilidad y pasión por el arte del café.</p>
+                    <h1>Misión</h1>
+                    <p>Ofrecer una experiencia de café excepcional que inspire y conecte, respetando los principios de calidad, sostenibilidad y pasión por el arte del café.</p><br/><br/>
+                    <h1>Visión</h1>
+                    <p>Queremos posicionarnos como la marca de referencia en el mundo del café artesanal, creando una comunidad apasionada y educada sobre el arte del café. Visualizamos un futuro en el que Bushido Beans sea sinónimo de calidad y responsabilidad, ofreciendo un espacio acogedor para que nuestros clientes vivan una experiencia única y memorable, mientras contribuimos al bienestar de nuestros colaboradores y la sostenibilidad del planeta.</p>
                 </div>
             </div>
 
-            <div className='container-vision'>
-                <p className='titulo titulo-vision'>Vision</p>
-                <p className='texto-vision'>Queremos posicionarnos como la marca de referencia en el mundo del café artesanal, creando una comunidad apasionada y educada sobre el arte del café. Visualizamos un futuro en el que Bushido Beans sea sinónimo de calidad y responsabilidad, ofreciendo un espacio acogedor para que nuestros clientes vivan una experiencia única y memorable, mientras contribuimos al bienestar de nuestros colaboradores y la sostenibilidad del planeta.</p>
-            </div>
-
+            <hr className='separador' />
 
             <div className='container-valores'>
-                <ul>
-                    <p className='titulo titulo-mision'>Valores</p>
-                    <li className='subtitulo texto-mision'><span className='spanBeans'>B</span>ienestar: Fomentamos un entorno positivo y saludable, tanto para nuestros colaboradores como para los clientes, apoyando prácticas que generen bienestar en toda la cadena de producción.</li>
-                    <li className='subtitulo texto-mision'><span className='spanBeans'>E</span>xcelencia: Nos comprometemos a ofrecer siempre el mejor café, desde el origen hasta el momento en que llega a la taza de cada cliente.</li>
-                    <li className='subtitulo texto-mision'><span className='spanBeans'>A</span>utenticidad: Valoramos y celebramos la esencia del café, respetando su autenticidad en cada paso de nuestro proceso.</li>
-                    <li className='subtitulo texto-mision'><span className='spanBeans'>N</span>aturaleza: Valoramos y respetamos el entorno natural, promoviendo prácticas que protejan los recursos y apoyen la biodiversidad en cada aspecto de nuestra operación.</li>
-                    <li className='subtitulo texto-mision'><span className='spanBeans'>S</span>ostenibilidad: Priorizamos prácticas responsables que respetan el entorno y apoyan un futuro más verde, reduciendo el impacto ambiental de nuestras operaciones.</li>
-                </ul>
-                <div className='container-imagen-valores' ><img src="/recursos/valores.jpg" alt="" className='imagen-valores' /></div>
+                <div className="container-texto-valores">
+                    <h1>Valores</h1>
+                    <p><span className='spanBeans'>B</span>ienestar: Fomentamos un entorno positivo y saludable, tanto para nuestros colaboradores como para los clientes, apoyando prácticas que generen bienestar en toda la cadena de producción.</p><br/><br/>
+                    <p><span className='spanBeans'>E</span>xcelencia: Nos comprometemos a ofrecer siempre el mejor café, desde el origen hasta el momento en que llega a la taza de cada cliente.</p><br/><br/>
+                    <p><span className='spanBeans'>A</span>utenticidad: Valoramos y celebramos la esencia del café, respetando su autenticidad en cada paso de nuestro proceso.</p><br/><br/>
+                    <p><span className='spanBeans'>N</span>aturaleza: Valoramos y respetamos el entorno natural, promoviendo prácticas que protejan los recursos y apoyen la biodiversidad en cada aspecto de nuestra operación.</p><br/><br/>
+                    <p><span className='spanBeans'>S</span>ostenibilidad: Priorizamos prácticas responsables que respetan el entorno y apoyan un futuro más verde, reduciendo el impacto ambiental de nuestras operaciones.</p>
+                    </div>
+                <img className='imagen-valores' src="/recursos/valores.jpg" alt=""/>
 
             </div>
 
