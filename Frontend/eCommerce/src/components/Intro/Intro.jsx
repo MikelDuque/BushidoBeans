5d792de092173dc091569d44f3efefc686d52579
MrAndrino <d.andfer96@gmail.com>
2024-12-11 10:52:10 +0100

    Intro Responsive

--- Frontend/eCommerce/src/components/Intro/Intro.jsx
+++ Frontend/eCommerce/src/components/Intro/Intro.jsx
@@ -7,13 +7,15 @@ const ScrollIntro = () => {
     useEffect(() => {
 
         document.body.style.overflow = 'hidden';
-        
+
 
         const timer = setTimeout(() => {
             window.scrollTo({ top: window.innerHeight, behavior: 'smooth' });
-            setTimeout(() => { setEsVisible(false);
-            window.scrollTo({top: 0});
-            document.body.style.overflow = ''; }, 450);     //Tiempo de animación
+            setTimeout(() => {
+                setEsVisible(false);
+                window.scrollTo({ top: 0 });
+                document.body.style.overflow = '';
+            }, 450);     //Tiempo de animación
         }, 3500);   //Delay antes de animación
 
         return () => clearTimeout(timer);
@@ -23,11 +25,20 @@ const ScrollIntro = () => {
 
     return (
         <div>
-            {esVisible && (
-                <div className={classes.introDiv}>
-                    <img className={classes.introImg} src='../recursos/LogoIntro.png'/>
-                </div>
-            )}
+            <div className={classes.introDisplay}>
+                {esVisible && (
+                    <div className={classes.introDiv}>
+                        <img className={classes.introImg} src='../recursos/LogoIntro.png' />
+                    </div>
+                )}
+            </div>
+            <div className={classes.introHidden}>
+                {esVisible && (
+                    <div className={classes.introDiv}>
+                        <img className={classes.introImg} src='../recursos/LogoD.png' />
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
