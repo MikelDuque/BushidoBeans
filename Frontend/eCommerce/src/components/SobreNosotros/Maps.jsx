597764eeaafb2011acd152848d81e2f0147e0a5a
MrAndrino <d.andfer96@gmail.com>
2024-12-12 11:39:07 +0100

    SN MediasQueries

--- Frontend/eCommerce/src/components/SobreNosotros/Maps.jsx
+++ Frontend/eCommerce/src/components/SobreNosotros/Maps.jsx
@@ -5,8 +5,8 @@ function Maps(direccion){
         <div className='map-container'>
             <iframe
                 src={direccion.direccion}
-                width="600"
-                height="450"
+                width="100%"
+                height="100%"
                 style={{ border: 0 }}
                 allowFullScreen=""
                 loading="lazy"
--- Frontend/eCommerce/src/pages/SobreNosotros/SobreNosotros.jsx
+++ Frontend/eCommerce/src/pages/SobreNosotros/SobreNosotros.jsx
@@ -25,8 +25,8 @@ function SobreNosotros() {
             <div className='container-descripcion'>
                 <img className='imagen-logo-bushido-beans' src={logo} alt="" />
                 <div className='descripcion-texto'>
-                    <p>En Bushido Beans, creemos que cada momento merece ser disfrutado con una taza de café o té de la más alta calidad. Fundada con la misión de ofrecer bebidas exquisitas y prácticas, nuestra marca está diseñada para aquellos que llevan un estilo de vida ajetreado y buscan un respiro en su día a día.</p><br/>
-                    <p>Nuestra selección incluye una variedad de cafés y tés cuidadosamente preparados, todos listos para beber y disfrutar en cualquier lugar y en cualquier momento. Ya sea que necesites un impulso de energía inmediata para afrontar una larga jornada, o simplemente desees saborear un momento de tranquilidad, Bushido Beans tiene la bebida perfecta para ti.</p><br/>
+                    <p>En Bushido Beans, creemos que cada momento merece ser disfrutado con una taza de café o té de la más alta calidad. Fundada con la misión de ofrecer bebidas exquisitas y prácticas, nuestra marca está diseñada para aquellos que llevan un estilo de vida ajetreado y buscan un respiro en su día a día.</p><br />
+                    <p>Nuestra selección incluye una variedad de cafés y tés cuidadosamente preparados, todos listos para beber y disfrutar en cualquier lugar y en cualquier momento. Ya sea que necesites un impulso de energía inmediata para afrontar una larga jornada, o simplemente desees saborear un momento de tranquilidad, Bushido Beans tiene la bebida perfecta para ti.</p><br />
                     <p>Cada producto que ofrecemos combina ingredientes de calidad premium con sabores únicos, presentados en formatos convenientes que se adaptan a tu ritmo de vida. Desde intensos cafés hasta delicados tés, nuestra variedad está diseñada para satisfacer todos los paladares.</p></div>
             </div>
 
@@ -36,7 +36,7 @@ function SobreNosotros() {
                 <img src="/recursos/mision.jpg" alt="" className='imagen-mision' />
                 <div className='mision-texto'>
                     <h1>Misión</h1>
-                    <p>Ofrecer una experiencia de café excepcional que inspire y conecte, respetando los principios de calidad, sostenibilidad y pasión por el arte del café.</p><br/><br/>
+                    <p>Ofrecer una experiencia de café excepcional que inspire y conecte, respetando los principios de calidad, sostenibilidad y pasión por el arte del café.</p><br /><br />
                     <h1>Visión</h1>
                     <p>Queremos posicionarnos como la marca de referencia en el mundo del café artesanal, creando una comunidad apasionada y educada sobre el arte del café. Visualizamos un futuro en el que Bushido Beans sea sinónimo de calidad y responsabilidad, ofreciendo un espacio acogedor para que nuestros clientes vivan una experiencia única y memorable, mientras contribuimos al bienestar de nuestros colaboradores y la sostenibilidad del planeta.</p>
                 </div>
@@ -47,69 +47,63 @@ function SobreNosotros() {
             <div className='container-valores'>
                 <div className="valores-texto">
                     <h1>Valores</h1>
-                    <p><span className='spanBeans'>B</span>ienestar: Fomentamos un entorno positivo y saludable, tanto para nuestros colaboradores como para los clientes, apoyando prácticas que generen bienestar en toda la cadena de producción.</p><br/><br/>
-                    <p><span className='spanBeans'>E</span>xcelencia: Nos comprometemos a ofrecer siempre el mejor café, desde el origen hasta el momento en que llega a la taza de cada cliente.</p><br/><br/>
-                    <p><span className='spanBeans'>A</span>utenticidad: Valoramos y celebramos la esencia del café, respetando su autenticidad en cada paso de nuestro proceso.</p><br/><br/>
-                    <p><span className='spanBeans'>N</span>aturaleza: Valoramos y respetamos el entorno natural, promoviendo prácticas que protejan los recursos y apoyen la biodiversidad en cada aspecto de nuestra operación.</p><br/><br/>
+                    <p><span className='spanBeans'>B</span>ienestar: Fomentamos un entorno positivo y saludable, tanto para nuestros colaboradores como para los clientes, apoyando prácticas que generen bienestar en toda la cadena de producción.</p><br /><br />
+                    <p><span className='spanBeans'>E</span>xcelencia: Nos comprometemos a ofrecer siempre el mejor café, desde el origen hasta el momento en que llega a la taza de cada cliente.</p><br /><br />
+                    <p><span className='spanBeans'>A</span>utenticidad: Valoramos y celebramos la esencia del café, respetando su autenticidad en cada paso de nuestro proceso.</p><br /><br />
+                    <p><span className='spanBeans'>N</span>aturaleza: Valoramos y respetamos el entorno natural, promoviendo prácticas que protejan los recursos y apoyen la biodiversidad en cada aspecto de nuestra operación.</p><br /><br />
                     <p><span className='spanBeans'>S</span>ostenibilidad: Priorizamos prácticas responsables que respetan el entorno y apoyan un futuro más verde, reduciendo el impacto ambiental de nuestras operaciones.</p>
-                    </div>
-                <img className='imagen-valores' src="/recursos/valores.jpg" alt=""/>
+                </div>
+                <img className='imagen-valores' src="/recursos/valores.jpg" alt="" />
             </div>
 
             <hr className='separador' />
 
             <div className='container-equipo'>
                 <div className="equipo-text">
-                <h1>Nuestro Equipo</h1>
-                <p>Nos une la pasión por la tecnología, el aprendizaje continuo, el café y el té, con el fin de mejorar la vida de nuestros clientes</p></div>
+                    <h1>Nuestro Equipo</h1>
+                    <p>Nos une la pasión por la tecnología, el aprendizaje continuo, el café y el té, con el fin de mejorar la vida de nuestros clientes</p></div>
 
                 <div className='container-cards'>
                     <Card nombre="Ivan Montes" cargo="Marketing" imagen={kirby}></Card>
                     <Card nombre="David Andrino" cargo="Logistica" imagen={sonic}></Card>
                     <Card nombre="Yasir Bel Maalem" cargo="Full Stack Developer" imagen={pikachu}></Card>
                     <Card nombre="Mikel Duque" cargo="CEO & Fundador" imagen={link}></Card>
-                </div> 
+                </div>
             </div>
 
             <hr className='separador' />
-            
 
-                <div className='container-informacionAdicional'>
-                    <div className='container-informacion'>
-                        <div className='container-dondeEstamos'>
-                            <p className='titulo'>Donde estamos</p>
-                            <hr className='separador-informacionAdicional' />
-                            <div className='container-direccion-contacto'>
-                                <p className='titulo'>Direcccion: </p> <span className='texto'>C. Charles Darwin, 3, Campanillas, 29590 Málaga</span>
-                                <p className='titulo'>Contacto: </p> <span className='texto'>+34 785 641 462 , bushidobeans@gmail.es</span>
-                            </div>
-                        </div>
+            <h1>Donde estamos</h1>
+            <div className='container-informacionAdicional'>
 
-                        <div className='container-redesSociales'>
-                            <p className='titulo'>Nuestras Redes</p>
-                            <hr className='separador-informacionAdicional' />
-                            <div className='container-img-redesSociales'>
-                                <div className="redes-sociales-item">
-                                    <img src={ig} alt="Instagram" />
-                                    <p className='subtitulo ig'>@bushidobeans</p>
-                                </div>
-                                <div className="redes-sociales-item">
-                                    <img src={tw} alt="Twitter" />
-                                    <p className='subtitulo tw'>@bushidobeans</p>
-                                </div>
-                                <div className="redes-sociales-item">
-                                    <img src={fc} alt="Facebook" />
-                                    <p className='subtitulo fc'>@bushidobeans</p>
-                                </div>
-                            </div>
-                        </div>
+                <div className='container-informacion'>
+                    <div className='dondeEstamos-texto'>
+                        <h1>Direcccion: </h1>
+                        <p>C. Charles Darwin, 3, Campanillas, 29590 Málaga</p>
+                        <h1>Contacto: </h1>
+                        <p>+34 785 641 462 , bushidobeans@gmail.es</p>
                     </div>
-                    <div className='container-maps'>
-                        <Maps className="maps" direccion="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3792.447613777691!2d-4.558384723732234!3d36.74307277091706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f1d3dde02b7b%3A0x499336c8cdfb308a!2sC.%20Charles%20Darwin%2C%203%2C%20Campanillas%2C%2029590%20M%C3%A1laga!5e1!3m2!1sen!2ses!4v1730662620262!5m2!1sen!2ses"></Maps>
+
+                    <div className='container-redesSociales'>
+                        <h1>Nuestras Redes</h1>
+                        <div className="redes-sociales-item">
+                            <img src={ig} alt="Instagram" />
+                            @bushidobeans
+                        </div>
+                        <div className="redes-sociales-item">
+                            <img src={tw} alt="Twitter" />
+                            @bushidobeans
+                        </div>
+                        <div className="redes-sociales-item">
+                            <img src={fc} alt="Facebook" />
+                            @bushidobeans
+                        </div>
                     </div>
                 </div>
+                <div><Maps className="maps" direccion="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3792.447613777691!2d-4.558384723732234!3d36.74307277091706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f1d3dde02b7b%3A0x499336c8cdfb308a!2sC.%20Charles%20Darwin%2C%203%2C%20Campanillas%2C%2029590%20M%C3%A1laga!5e1!3m2!1sen!2ses!4v1730662620262!5m2!1sen!2ses" /></div>
             </div>
-        
+        </div>
+
     );
 
 }
